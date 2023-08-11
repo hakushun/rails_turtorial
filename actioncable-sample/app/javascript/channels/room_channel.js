01d6d0e0b1ae07f48d3c07526c543edb1749688f
hakushun <hakushun.pianist@gmail.com>
2023-08-11 17:03:45 +0900

    feat: create actioncable-sample

---
+++ actioncable-sample/app/javascript/channels/room_channel.js
@@ -0,0 +1,27 @@
+import consumer from "channels/consumer";
+
+consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    document.querySelector('input[data-behavior="room_speaker"]').addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        this.speak(event.target.value);
+        event.target.value = "";
+        return event.preventDefault();
+      }
+    });
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    const element = document.querySelector("#messages");
+    element.insertAdjacentHTML("beforeend", data["message"]);
+  },
+
+  speak: function (message) {
+    return this.perform("speak", { message: message });
+  },
+});
